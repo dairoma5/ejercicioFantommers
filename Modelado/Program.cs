fdce3fe58fd3522b12bc4f411390cef56d1fada9
Daiana <dairoma5@gmail.com>
2020-10-25 14:36:30 -0300

    se creó la clase persona

--- Modelado/Program.cs
+++ Modelado/Program.cs
@@ -10,6 +10,22 @@ namespace Modelado
     {
         static void Main(string[] args)
         {
+            int counter = 0;
+            string line;
+
+            // Leo el archivo TestDaiana.txt 
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"C:\Users\54934\Desktop\Daiana\EjercicioFantommer\TestDaiana.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                //System.Console.WriteLine(line);
+                counter++;
+            }
+
+            file.Close();
+            //System.Console.WriteLine("There were {0} lines.", counter);
+            // Suspend the screen.  
+            System.Console.ReadLine();
         }
     }
 }
